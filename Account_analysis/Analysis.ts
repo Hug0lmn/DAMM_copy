06d02a96684e751a66433370a2c61369bdc089ab
unknown <Lemonnier.h@outlook.com>
2025-07-02 16:23:47 +0200

    Improve usability

--- Account_analysis/Analysis.ts
+++ Account_analysis/Analysis.ts
@@ -1,24 +1,43 @@
 import 'dotenv/config';
 import { Get_signatures, Trans_pool, Trans_wallet } from './Functions';
 
-let adress = "FkKyp6JBXDcFqFCHULr1JYqrTsck1D9giQfRm2CBKcxA"; //
 const Helius_API_KEY = process.env.API_KEY;
-const filename = "OUIOUI";
-const type_ = "pool";
+const args = process.argv.slice(2);
+console.log(args);
+
+if (args.length !== 1) {
+    console.error("Usage: node script.js <wallet|pool> <adress> <filename>");
+    process.exit(1);
+}
+
+const type_ = args[0];
+let adress = args[1];
+const filename = args[2];
+
+if (type_ !== "wallet" && type_ !== "pool") {
+    console.error(`Invalid argument: ${type_}. Use "wallet" or "pool".`);
+    process.exit(1);
+} else if (typeof adress === 'string') {
+    console.error(`Invalid argument: ${adress}. Use string type.`);
+    process.exit(1);
+} else if (typeof filename === 'string') {
+    console.error(`Invalid argument: ${filename}. Use string type.`);
+    process.exit(1);
+}
 
 (async()=>{
     if (type_ == "wallet"){
-        await Get_signatures(adress, Helius_API_KEY!, 10000, filename);
+        await Get_signatures(adress, Helius_API_KEY!, 10000);
         console.log(`Analyzing ${adress} wallet transactions...`);
         await Trans_wallet(Helius_API_KEY!, filename,adress);
     }
     else if (type_ == "pool"){
-        await Get_signatures(adress, Helius_API_KEY!, 500000, filename);
+        await Get_signatures(adress, Helius_API_KEY!, 500000);
         console.log(`Analyzing ${adress} pool transactions...`);
         const profitable_wallet = await Trans_pool(Helius_API_KEY!, filename, adress);
 
         for (const wallet_adress of profitable_wallet){//Analyzing the most profitable wallet
-            await Get_signatures(wallet_adress, Helius_API_KEY!, 10000, filename);
+            await Get_signatures(wallet_adress, Helius_API_KEY!, 10000);
             console.log(`Analyzing ${wallet_adress} wallet transactions...`);
             await Trans_wallet(Helius_API_KEY!, filename, adress);
         }
--- Account_analysis/README.md
+++ Account_analysis/README.md
@@ -14,9 +14,6 @@ This folder contains code for collecting and analyzing data on DAMM v2 pools and
 - **Implement Date-Based Filtering**  
   Add options to filter wallet transactions or signatures by a specified date range, enabling more targeted analysis over specific time periods.
 
-- **Improve Usability**  
-  Allow parameters (e.g., addresses, analysis type, output name) to be provided via command-line arguments, avoiding the need to manually modify the script for each run.
-
 - **Create a Personal Database**  
   In addition to saving analysis results in JSON files, store them in a Supabase database for better organization, retrieval, and future analysis.
 
@@ -32,7 +29,4 @@ This folder contains code for collecting and analyzing data on DAMM v2 pools and
 
 1. Open `Analysis.ts`.
 2. Add your private Helius API key.
-3. Specify the address you want to analyze.
-4. Modify the JSON output filename as needed.
-5. Choose the type of analysis you want to perform.
-6. Run `Analysis.ts`.
+3. Run `Analysis.ts` with the parameter in the command line such as : npx tsx Analysis.ts wallet adress filename
